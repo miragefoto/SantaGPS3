eb9dffe02c37468a4d1a03735e0a61e56380454e
Eric Phillips <eric.phillips@thunderbolttech.net>
2018-12-17 16:14:54 -0500

    update

---
+++ SantaBG3/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LocationTracker } from '../providers/location-tracker/location-tracker';
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
+import { Geolocation } from '@ionic-native/geolocation';
+//import { HttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    BackgroundGeolocation,
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocationTracker,
+    //HttpClient
+  ]
+})
+export class AppModule {}
